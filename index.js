b5f944fc785601bf4404dce1e303af7575cd653b
Алешенька:3 <alesenka@MacBook-Air-Alesenka3.local>
2018-09-23 16:34:35 +0300

    heroku deploy

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ bot.onText(/\/chat/, (msg) => {
 bot.on('message', function (msg) {
     var chatId = msg.chat.id;
     if (msg.contact) {
-        console.log((msg.contact.phone_number[0]))
         if(msg.contact.phone_number[0]!=="+"){
-            msg.contact.phone_number[0] = '+'+ msg.contact.phone_number[0]
+            msg.contact.phone_number = '+'+ msg.contact.phone_number
         }
+        console.log()
         database.updateData('users/' + chatId, msg.contact);
         sendIdAsk(chatId)
     }
